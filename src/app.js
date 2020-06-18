7b942d2091ef1ff52f71a24ca1b12e8a140edf49
Wayne Wang <wayne.wang@tetrapak.com>
2020-06-18 20:44:35 +0800

    koa-router and dummy example

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
-const koa = require("koa");
-const app = new koa();
+const Koa = require("koa");
+const Router = require("@koa/router");
 
-app.use((context) => {
-  context.body = "ready";
+const app = new Koa();
+const router = new Router();
+
+router.get("/example", (ctx, next) => {
+  var response = require("./data/example.json");
+  ctx.body = response;
 });
+
+app.use(router.routes()).use(router.allowedMethods());
 
 module.exports = app;
